3d431ce7759cf8a39f60f4acfd9ad7f0d0d3fc30
ChristineAdib <christineadib8@gmail.com>
2026-02-22 00:29:37 +0200

    Add Project Architecture

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = DESKTOP-LINP2LF; Initial Catalog = ECommerceDb; Integrated Security = True; Encrypt = False;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
