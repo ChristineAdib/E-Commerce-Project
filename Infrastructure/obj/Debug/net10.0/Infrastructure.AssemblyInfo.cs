755132b807756de2861a341ecc9122b4dc01e40d
Afaf Rady <afaf@example.com>
2026-02-24 12:11:38 +0200

    handle conflicts

--- Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67b9739479efd7139c44fe4a68670d4c4330682d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7d7b3a3bf112b0afc2a9ac581640a1ba1011ea8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
