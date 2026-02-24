7d7f8c09537d5da8db17d30adad46002455a626e
Afaf Rady <afaf@example.com>
2026-02-24 12:22:10 +0200

    handle conflicts

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67b9739479efd7139c44fe4a68670d4c4330682d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ec445187c3aab8809997a68dc656f264b45c266")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7d7b3a3bf112b0afc2a9ac581640a1ba1011ea8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ec445187c3aab8809997a68dc656f264b45c266")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
